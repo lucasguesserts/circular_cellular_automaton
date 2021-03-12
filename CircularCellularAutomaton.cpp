d382045d6c21c79cab1c78261bb7819c91d8a766
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2021-03-11 21:37:40 -0300

    class CircularCellularAutomaton complete

--- CircularCellularAutomaton.cpp
+++ CircularCellularAutomaton.cpp
@@ -4,10 +4,12 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <stdexcept>
+#include <cmath>
 
 std::string to_string(std::vector<unsigned> vec)
 {
-    std::string str;
+    std::string str = "";
     str += "{";
     for(auto it=vec.cbegin() ; it<vec.cend()-1 ; ++it)
     {
@@ -31,12 +33,44 @@ class CircularCellularAutomaton
               cellsOrder(cellsOrder),
               environmentDistance(environmentDistance)
             {
-                this->values = initialValues;
+                if (initialValues.size() != automatonOrder)
+                    throw std::invalid_argument("automaton order and amount of initial values differ");
+                else
+                    this->values = initialValues;
                 return;
             }
+        void step(void)
+        {
+            auto newValues = this->values;
+            for(unsigned cell=0 ; cell<this->automatonOrder ; ++cell)
+            {
+                newValues[cell] = this->computeNewValue(cell);
+            }
+            this->values = newValues;
+            return;
+        }
+        unsigned computeNewValue(const unsigned cell)
+        {
+            unsigned accumulator = 0;
+            for(unsigned otherCell=0 ; otherCell<this->automatonOrder ; ++otherCell)
+            {
+                if (this->computeDistance(cell, otherCell) < this->environmentDistance)
+                {
+                    accumulator += this->values[otherCell];
+                }
+            }
+            return accumulator;
+        }
+        unsigned computeDistance(const unsigned lhs, const unsigned rhs)
+        {
+            return std::fmin(
+                std::fabs(lhs - rhs),
+                this->automatonOrder - std::fabs(lhs - rhs)
+            );
+        }
         operator std::string() const
         {
-            std::string str;
+            std::string str = "";
             str += std::string("Circular Cellular Automaton")                                         + std::string("\n") + \
                    std::string("---------------------------")                                         + std::string("\n") + \
                    std::string("automaton order      = ") + std::to_string(this->automatonOrder)      + std::string("\n") + \
@@ -57,6 +91,10 @@ class CircularCellularAutomaton
 int main ()
 {
     CircularCellularAutomaton automaton(5, 3, 2, {0, 1, 2, 0, 1});
+    for(unsigned k=0 ; k<5 ; ++k)
+    {
+        automaton.step();
+    }
     std::cout << std::string(automaton) << std::endl;
     return 0;
 }
