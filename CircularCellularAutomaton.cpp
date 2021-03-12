8bda435d11d44fbe69121a6697f5f2280a619099
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2021-03-11 21:14:35 -0300

    Add constructor to CircularCellularAutomaton and a basic test

--- CircularCellularAutomaton.cpp
+++ CircularCellularAutomaton.cpp
@@ -5,6 +5,20 @@
 #include <string>
 #include <iostream>
 
+std::string to_string(std::vector<unsigned> vec)
+{
+    std::string str;
+    str += "{";
+    for(auto it=vec.cbegin() ; it<vec.cend()-1 ; ++it)
+    {
+        ;
+        str += std::to_string(*it) + std::string(", ");
+    }
+    str += std::to_string(vec.back());
+    str += "}";
+    return str;
+}
+
 class CircularCellularAutomaton
 {
     public:
@@ -20,6 +34,19 @@ class CircularCellularAutomaton
                 this->values = initialValues;
                 return;
             }
+        operator std::string() const
+        {
+            std::string str;
+            str += std::string("Circular Cellular Automaton")                                         + std::string("\n") + \
+                   std::string("---------------------------")                                         + std::string("\n") + \
+                   std::string("automaton order      = ") + std::to_string(this->automatonOrder)      + std::string("\n") + \
+                   std::string("cells order          = ") + std::to_string(this->cellsOrder)          + std::string("\n") + \
+                   std::string("environment distance = ") + std::to_string(this->environmentDistance) + std::string("\n") + \
+                   std::string("values               = ") + to_string(this->values)                   + std::string("\n") + \
+                   std::string("---------------------------")                                         + std::string("\n") ;
+            return str;
+        }
+
     private:
         const unsigned        automatonOrder;
         const unsigned        cellsOrder;
@@ -29,6 +56,8 @@ class CircularCellularAutomaton
 
 int main ()
 {
+    CircularCellularAutomaton automaton(5, 3, 2, {0, 1, 2, 0, 1});
+    std::cout << std::string(automaton) << std::endl;
     return 0;
 }
 
